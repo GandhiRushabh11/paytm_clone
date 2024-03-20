b3f7f13c780266e85d98e78ddeb47366e3be41ad
GandhiRushabh11 <gandhirushabh11@gmail.com>
2024-03-20 23:56:30 +0530

    Added initial balance for account '

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -1,10 +1,10 @@
 const userModel = require("../models/user");
-
+const accountModel = require("../models/account");
 exports.register = async (req, res) => {
   const { username, firstName, lastName, password, email } = req.body;
 
   let user = await userModel.findOne({ email });
-
+  const initialBal = Math.floor(Math.random() * 100000);
   if (user) {
     return res.json({
       success: false,
@@ -19,13 +19,22 @@ exports.register = async (req, res) => {
     email,
   });
 
+  const accountData = await accountModel.create({
+    userId: user._id,
+    balance: initialBal,
+  });
   const token = user.getSignedjwtToken();
   res
     .status(200)
-    .json({ success: true, message: "User created successfully", token });
+    .json({
+      success: true,
+      message: "User created successfully",
+      token,
+      balance: accountData.balance,
+    });
 };
 
-exports.sigin = async (req, res) => {
+exports.signin = async (req, res) => {
   const { username, password } = req.body;
 
   let user = await userModel.findOne({ username });
@@ -48,4 +57,33 @@ exports.sigin = async (req, res) => {
 
   const token = user.getSignedjwtToken();
   res.status(200).json({ success: true, token });
+};
+
+exports.updateUser = async (req, res) => {
+  const { firstName, lastName, password } = req.body;
+  const user = req.user;
+  const updatedUser = await userModel.findByIdAndUpdate(
+    user._id,
+    {
+      firstName,
+      lastName,
+      password,
+    },
+    { new: true, runValidators: true }
+  );
+  res.json({
+    success: true,
+    message: "Updated successfully",
+    data: updatedUser,
+  });
+};
+
+exports.getUsers = async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await userModel.find({
+    $or: [{ firstName: { $regex: filter } }, { lastName: { $regex: filter } }],
+  });
+
+  res.json({ success: true, data: users });
 };
